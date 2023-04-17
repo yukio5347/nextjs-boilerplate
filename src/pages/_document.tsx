016244908972e0affd8f73d8eb906e6357e15d53
Yukio <yukio5347@gmail.com>
2023-04-17 12:12:42 +0900

    remove contents in src/pages/index.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='font-sans bg-white text-gray-700'>
         <Main />
         <NextScript />
       </body>
