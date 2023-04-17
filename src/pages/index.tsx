f0d60ad5fa05a0b72964508705d1b5861613e07d
Yukio <yukio5347@gmail.com>
2023-04-17 17:10:02 +0900

    run lint to all tsx files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="container bg-blue-100">
-      /src/pages/index.tsx
-    </main>
-  )
+  return <main className='container bg-blue-100'>/src/pages/index.tsx</main>;
 }
